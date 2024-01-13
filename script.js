ee9ee0cd1f5db62a390c78a43f3a4cb0a8062d5b
Diego Javier Soto <109754442+DiegoJavierSoto@users.noreply.github.com>
2024-01-13 12:04:12 -0300

    Add files via upload
    Faltan corregir foto de perfil y agregar proyectos

---
+++ script.js
@@ -0,0 +1,24 @@
+//funcion que aplica el estilo a la opción seleccionada en el menú y quita la previamente seleccionada
+function seleccionar(link){
+    var opciones = document.querySelectorAll('#links a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+    //Hacemos desaparecer el menú una vez que se ha seleccionado una opción en el modo responsive
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+//funcion que muestra el menu responsive
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive";
+    }else{
+        x.className = "";
+    }
+}
+
